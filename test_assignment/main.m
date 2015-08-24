b90bdb2337d9351c7fae1ef20849da5d45680d03
mac <mac@macs-MacBook-Pro.local>
2015-08-24 11:53:32 +0530

    Initial Commit

---
+++ test_assignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test_assignment
+//
+//  Created by mac on 8/24/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
